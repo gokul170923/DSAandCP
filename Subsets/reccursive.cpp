c770455d72ad6924be76b031c76a84a570c30d49
gokul170923 <145271997+gokul170923@users.noreply.github.com>
2024-01-03 09:22:03 +0530

    learned subsets

---
+++ Subsets/reccursive.cpp
@@ -0,0 +1,35 @@
+// using Reccursion make choice
+
+#include <bits/stdc++.h>
+using namespace std;
+void print(vector<int>&arr){
+    cout<<"[";
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<",";
+    }
+    cout<<"]"<<endl;
+}
+
+void recc(vector<int>arr,vector<int>temp,int i,int n){
+    if(i>=n){
+        print(temp);
+        return;
+    }
+    recc(arr,temp,i+1,n);
+    temp.push_back(arr[i]);
+    recc(arr,temp,i+1,n);
+   
+    
+
+
+}
+
+
+
+int main(){
+    vector<int>arr={3,1,2},temp;
+    recc(arr,temp,0,3);
+    
+
+    return 0;
+}
